b45fdf4dceda5f99f7e3ca888a05e0db684a0b86
Arslan Wali <arsalwali7@gmail.com>
2025-08-24 18:33:20 +0500

    haider_web

---
+++ resources/views/frontend/components/sticky_header.blade.php
@@ -0,0 +1,291 @@
+
+		<!-- Header Sticky -->
+
+		<div class="tq-sticky-menu">
+			<div class="container">
+				<div class="tq-sticky-menu-inner d-flex flex-wrap align-items-center justify-content-between w-100">
+					<div class="tq-logo position-relative w-100">
+						<a href="https://naudummy.com/muezzin/"><svg class="fill-theme"
+								xmlns="http://www.w3.org/2000/svg" viewBox="0 0 184.15 191.11">
+								<g>
+									<path d="M93.08,191.11c-0.33,0-0.67,0-1,0c-5.44-8.58-13.89-13.21-22.85-16.97c-9.31-3.91-17.25-9.39-22.27-18.33
+									c-1.76-3.14-3.91-4.36-7.49-4.54C27.15,150.68,20,144.64,17,132.6c-0.51-2.04-1.41-3.36-3.43-3.73c-5.72-1.05-9.92-4.41-13.5-8.75
+									C0.08,81.49,0.12,42.87,0,4.25C-0.01,1.13,0.22-0.01,3.93,0c58.76,0.16,117.53,0.16,176.29,0c3.71-0.01,3.94,1.13,3.93,4.25
+									c-0.12,38.29-0.08,76.58-0.08,114.86c-3.3,4.84-7.56,8.35-13.42,9.66c-2.18,0.49-3.38,1.86-3.81,4.08
+									c-2.43,12.48-11.13,17.35-22.76,18.52c-2.09,0.21-4,0.61-5.04,2.62c-5.68,11.04-15.46,16.87-26.43,21.58
+									C104.81,178.91,97.44,183.23,93.08,191.11z"></path>
+								</g>
+							</svg><img src="images/logo-insignia.png.webp" alt="" loading="lazy"></a>
+					</div><!-- Logo -->
+					<nav>
+						<ul id="menu-main-menu-1" class="">
+							<li
+								class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children menu-item-635">
+								<a>Home</a>
+								<ul class="sub-menu">
+									<li
+										class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-2330 current_page_item menu-item-2332">
+										<a href="https://naudummy.com/muezzin/" aria-current="page">Home – Style 1</a>
+									</li>
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2969">
+										<a href="https://naudummy.com/muezzin/islamic/">Home – Style 2</a>
+									</li>
+								</ul>
+							</li>
+							<li
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-693">
+								<a>Charity</a>
+								<ul class="sub-menu">
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-707">
+										<a href="https://naudummy.com/muezzin/our-campaigns/">Our Campaigns</a>
+									</li>
+									<li
+										class="menu-item menu-item-type-post_type menu-item-object-campaign menu-item-783">
+										<a href="https://naudummy.com/muezzin/campaigns/educate-rural-kenya/">Campaign
+											Details</a>
+									</li>
+								</ul>
+							</li>
+							<li
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-724">
+								<a href="#">Events</a>
+								<ul class="sub-menu">
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-723">
+										<a href="https://naudummy.com/muezzin/upcoming-events/">Our Events</a>
+									</li>
+									<li
+										class="menu-item menu-item-type-post_type menu-item-object-event menu-item-2038">
+										<a href="https://naudummy.com/muezzin/events/intl-hajj-program-offers/">Intl.
+											Hajj Program Offers</a>
+									</li>
+								</ul>
+							</li>
+							<li
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-630">
+								<a>Scholars</a>
+								<ul class="sub-menu">
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-671">
+										<a href="https://naudummy.com/muezzin/islamic-scholars/">Islamic Scholars</a>
+									</li>
+									<li class="menu-item menu-item-type-post_type menu-item-object-team menu-item-796">
+										<a href="https://naudummy.com/muezzin/team/sharif-hammam/">Scholar Profile</a>
+									</li>
+								</ul>
+							</li>
+							<li
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-628">
+								<a>Pages</a>
+								<ul class="sub-menu">
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-972">
+										<a href="https://naudummy.com/muezzin/about-us/">About Us</a>
+									</li>
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3366">
+										<a href="https://naudummy.com/muezzin/online-classes/">Online Classes</a>
+									</li>
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3375">
+										<a href="https://naudummy.com/muezzin/travel-tours/">Travel & Tours</a>
+									</li>
+									<li
+										class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-632">
+										<a>Services</a>
+										<ul class="sub-menu">
+											<li
+												class="menu-item menu-item-type-post_type menu-item-object-page menu-item-673">
+												<a href="https://naudummy.com/muezzin/offered-services/">Offered
+													Services</a>
+											</li>
+											<li
+												class="menu-item menu-item-type-post_type menu-item-object-services menu-item-801">
+												<a href="https://naudummy.com/muezzin/services/hajj-umrah/">Service
+													Details</a>
+											</li>
+										</ul>
+									</li>
+									<li
+										class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-689">
+										<a>Blog Posts</a>
+										<ul class="sub-menu">
+											<li
+												class="menu-item menu-item-type-post_type menu-item-object-page menu-item-703">
+												<a href="https://naudummy.com/muezzin/latest-blog/">Blog Style –
+													List</a>
+											</li>
+											<li
+												class="menu-item menu-item-type-post_type menu-item-object-page menu-item-702">
+												<a href="https://naudummy.com/muezzin/grid-blog-posts/">Blog Style –
+													Grid</a>
+											</li>
+											<li
+												class="menu-item menu-item-type-post_type menu-item-object-post menu-item-790">
+												<a href="https://naudummy.com/muezzin/is-islam-a-modern-philosophy/">Blog
+													Details</a>
+											</li>
+										</ul>
+									</li>
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2114">
+										<a href="https://naudummy.com/muezzin/audios/">Audio Sermons</a>
+									</li>
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3433">
+										<a href="https://naudummy.com/muezzin/salat-timings/">Salat Timings</a>
+									</li>
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-675">
+										<a href="https://naudummy.com/muezzin/islamic-teachings/">Islamic Teachings</a>
+									</li>
+									<li
+										class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-3451">
+										<a href="https://naudummy.com/muezzin/category/blog-list/">Most Searched</a>
+									</li>
+								</ul>
+							</li>
+							<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-674"><a
+									href="https://naudummy.com/muezzin/contact-us/">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div><!-- Sticky Header -->
+
+
+		<div class="tq-res-header position-relative w-100">
+			<div class="tq-res-topbar w-100">
+				<div class="container">
+					<div class="tq-res-topbar-inner d-flex flex-wrap align-items-center justify-content-between w-100">
+						<div class="tq-logo position-relative w-100">
+							<a href="https://naudummy.com/muezzin/">
+								<svg class="fill-theme" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 184.15 191.11">
+									<g>
+										<path d="M93.08,191.11c-0.33,0-0.67,0-1,0c-5.44-8.58-13.89-13.21-22.85-16.97c-9.31-3.91-17.25-9.39-22.27-18.33
+                                        c-1.76-3.14-3.91-4.36-7.49-4.54C27.15,150.68,20,144.64,17,132.6c-0.51-2.04-1.41-3.36-3.43-3.73c-5.72-1.05-9.92-4.41-13.5-8.75
+                                        C0.08,81.49,0.12,42.87,0,4.25C-0.01,1.13,0.22-0.01,3.93,0c58.76,0.16,117.53,0.16,176.29,0c3.71-0.01,3.94,1.13,3.93,4.25
+                                        c-0.12,38.29-0.08,76.58-0.08,114.86c-3.3,4.84-7.56,8.35-13.42,9.66c-2.18,0.49-3.38,1.86-3.81,4.08
+                                        c-2.43,12.48-11.13,17.35-22.76,18.52c-2.09,0.21-4,0.61-5.04,2.62c-5.68,11.04-15.46,16.87-26.43,21.58
+                                        C104.81,178.91,97.44,183.23,93.08,191.11z"></path>
+									</g>
+								</svg>
+								<img width="216" height="102" src="images/logo2.png" alt="Muezzin" itemprop="image">
+							</a>
+						</div><!-- Logo -->
+
+						<div class="tq-res-header-right d-inline-flex align-items-center">
+							<div class="tq-social-links text-white d-inline-flex align-items-center">
+
+								<a href="https://twitter.com" title="" itemprop="url" target="_blank"><i
+										class="fab fa-twitter"></i></a>
+								<a href="https://facebook.com" title="" itemprop="url" target="_blank"><i
+										class="fab fa-facebook-f"></i></a>
+
+								<a href="https://linkedin.com" title="" itemprop="url" target="_blank"><i
+										class="fab fa-linkedin-in"></i></a>
+								<a href="https://instagram.com" title="" itemprop="url" target="_blank"><i
+										class="fab fa-instagram"></i></a>
+							</div><!-- Social Links -->
+							<a class="tq-res-menu-trigger" href="javascript:void(0);" title=""><i
+									class="fa fa-align-justify"></i></a>
+						</div><!-- Header Right -->
+
+
+					</div><!-- Responsive Topbar Inner -->
+				</div>
+			</div><!-- Responsive Topbar -->
+			<div class="tq-res-menu position-fixed w-100 rsnp-mnu">
+				<a class="tq-res-menu-close position-absolute" href="javascript:void(0);"><i
+						class="fa fa-times"></i></a>
+				<ul id="menu-main-menu-2" class="">
+					<li
+						class="menu-item menu-item-type-custom menu-item-object-custom current-menu-ancestor current-menu-parent menu-item-has-children menu-item-635">
+						<a>Home</a>
+						<ul class="sub-menu">
+							<li
+								class="menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-2330 current_page_item menu-item-2332">
+								<a href="https://naudummy.com/muezzin/" aria-current="page">Home – Style 1</a>
+							</li>
+							<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2969"><a
+									href="https://naudummy.com/muezzin/islamic/">Home – Style 2</a></li>
+						</ul>
+					</li>
+					<li
+						class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-693">
+						<a>Charity</a>
+						<ul class="sub-menu">
+							<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-707"><a
+									href="https://naudummy.com/muezzin/our-campaigns/">Our Campaigns</a></li>
+							<li class="menu-item menu-item-type-post_type menu-item-object-campaign menu-item-783"><a
+									href="https://naudummy.com/muezzin/campaigns/educate-rural-kenya/">Campaign
+									Details</a></li>
+						</ul>
+					</li>
+					<li
+						class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-724">
+						<a href="#">Events</a>
+						<ul class="sub-menu">
+							<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-723"><a
+									href="https://naudummy.com/muezzin/upcoming-events/">Our Events</a></li>
+							<li class="menu-item menu-item-type-post_type menu-item-object-event menu-item-2038"><a
+									href="https://naudummy.com/muezzin/events/intl-hajj-program-offers/">Intl. Hajj
+									Program Offers</a></li>
+						</ul>
+					</li>
+					<li
+						class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-630">
+						<a>Scholars</a>
+						<ul class="sub-menu">
+							<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-671"><a
+									href="https://naudummy.com/muezzin/islamic-scholars/">Islamic Scholars</a></li>
+							<li class="menu-item menu-item-type-post_type menu-item-object-team menu-item-796"><a
+									href="https://naudummy.com/muezzin/team/sharif-hammam/">Scholar Profile</a></li>
+						</ul>
+					</li>
+					<li
+						class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-628">
+						<a>Pages</a>
+						<ul class="sub-menu">
+							<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-972"><a
+									href="https://naudummy.com/muezzin/about-us/">About Us</a></li>
+							<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3366"><a
+									href="https://naudummy.com/muezzin/online-classes/">Online Classes</a></li>
+							<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3375"><a
+									href="https://naudummy.com/muezzin/travel-tours/">Travel & Tours</a></li>
+							<li
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-632">
+								<a>Services</a>
+								<ul class="sub-menu">
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-673">
+										<a href="https://naudummy.com/muezzin/offered-services/">Offered Services</a>
+									</li>
+									<li
+										class="menu-item menu-item-type-post_type menu-item-object-services menu-item-801">
+										<a href="https://naudummy.com/muezzin/services/hajj-umrah/">Service Details</a>
+									</li>
+								</ul>
+							</li>
+							<li
+								class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-689">
+								<a>Blog Posts</a>
+								<ul class="sub-menu">
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-703">
+										<a href="https://naudummy.com/muezzin/latest-blog/">Blog Style – List</a>
+									</li>
+									<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-702">
+										<a href="https://naudummy.com/muezzin/grid-blog-posts/">Blog Style – Grid</a>
+									</li>
+									<li class="menu-item menu-item-type-post_type menu-item-object-post menu-item-790">
+										<a href="https://naudummy.com/muezzin/is-islam-a-modern-philosophy/">Blog
+											Details</a>
+									</li>
+								</ul>
+							</li>
+							<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-2114"><a
+									href="https://naudummy.com/muezzin/audios/">Audio Sermons</a></li>
+							<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-3433"><a
+									href="https://naudummy.com/muezzin/salat-timings/">Salat Timings</a></li>
+							<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-675"><a
+									href="https://naudummy.com/muezzin/islamic-teachings/">Islamic Teachings</a></li>
+							<li class="menu-item menu-item-type-taxonomy menu-item-object-category menu-item-3451"><a
+									href="https://naudummy.com/muezzin/category/blog-list/">Most Searched</a></li>
+						</ul>
+					</li>
+					<li class="menu-item menu-item-type-post_type menu-item-object-page menu-item-674"><a
+							href="https://naudummy.com/muezzin/contact-us/">Contact</a></li>
+				</ul>
+			</div><!-- Responsive Menu -->
+		</div><!-- Responsive Header -->
---
+++ resources/views/frontend/website/website.blade.php
@@ -0,0 +1,10 @@
+@extends("frontend.layout.app")
+@section('main')
+
+@include('frontend.components.header')
+@include('frontend.components.sticky_header')
+@include('frontend.website.index')
+@include('frontend.components.footer')
+
+
+@endsection
